22c3d554ac99b173fa61d8ba5d6994db88093272
Kristinn <kthk8@hi.is>
2021-02-03 15:52:46 +0000

    útfæra "lazyLoad" með intersectionObserver

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const targets = document.querySelectorAll('img');
 
-window.addEventListener('scroll', (event) => {
-    targets.forEach( img => {
-        console.log('💩');
-        const rect = img.getBoundingClientRect().top;
-        if (rect <= window.innerHeight ) {
-            const src = img.getAttribute('data-lazy');
-            img.setAttribute('src', src);
-            img.classList.add('fade-in');
-        };
+const lazyLoad = (target) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      console.info('😍');
+
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute('data-lazy');
+
+        img.setAttribute('src', src);
+        img.classList.add('fade-in');
+
+        observer.disconnect();
+      }
     });
-});
+  });
 
-const express = require('express');
+  io.observe(target);
+};
 
-const x = 1;
+targets.forEach(lazyLoad);
