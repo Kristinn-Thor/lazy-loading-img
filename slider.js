912849ea3910f812af1627f62d1ef649383cebe5
Kristinn <kthk8@hi.is>
2021-02-11 11:02:51 +0000

    laga vh css bug fyrir ios tæki

--- slider.js
+++ slider.js
@@ -1,3 +1,11 @@
+// Fix for ios mobile browser-bottom-navigation-bar
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 const slider = document.querySelector('.slider-container');
 const slides = Array.from(document.querySelectorAll('.slide'));
 const leftArrow = document.querySelector('#left');
